11a0effdd3f1e7883be2e6329cf183ec33366634
QuantumX Labs <samuraisoulstudio.mbs@gmail.com>
2025-02-21 02:16:02 +0530

    QuantumX Labs-Webpage
    QuantumX Labs is not just another blockchain project—it’s a forward-thinking solution designed to secure the digital future. By addressing the imminent threats posed by quantum computing and overcoming traditional blockchain limitations, we are setting the stage for a new era of secure, decentralized technology.

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        'accent-green': 'rgb(var(--color-accent-green) / <alpha-value>)',
+        'accent-purple': 'rgb(var(--color-accent-purple) / <alpha-value>)',
+        'accent-blue': 'rgb(var(--color-accent-blue) / <alpha-value>)',
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
